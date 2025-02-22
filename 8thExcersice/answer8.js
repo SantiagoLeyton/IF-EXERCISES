d37c4aac0da3c9982c8d610cf1b82b3f54a0c8a5
Santiago Leyton <sleyton_644@unihumboldt.edu.co>
2025-02-22 01:40:31 -0500

    Added eighth excersice

---
+++ 8thExcersice/answer8.js
@@ -0,0 +1,26 @@
+let time8;
+let monthly;
+
+while (true) {
+    time8 = parseInt(prompt(`How do you wanna pay your gym membership?
+1) every 15 days (18,000)
+2) every 30 days (35,000)
+3) every 3 months (86,000)`))
+    if(time8 == 1) {
+        monthly = 18000*2;
+        break;
+    }else if(time8 == 2) {
+        monthly = 35000;
+        break;
+    }else if(time8 == 3) {
+        monthly = 86000/3;
+        break;
+    };
+
+    alert('Please select an option')
+};
+
+monthly = monthly.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
+document.getElementById('EighthAns').innerHTML = `<h2>WORLD GYM</h2>
+<p>You are paying ${monthly} every month</p>`
